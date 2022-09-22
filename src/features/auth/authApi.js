cc35521da79854d6989e743c2d688d260c046a7b
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-22 20:15:08 +0600

    implemented auth and slice

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,69 @@
 import { apiSlice } from '../api/apiSlice';
+import { userLoggedIn } from './authSlice';
 
 export const authApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    // endpoints here
+    // register endpoint
+    register: builder.mutation({
+      query: (data) => ({
+        url: '/register',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          // do nothing
+        }
+      },
+    }),
+    // login endpoint
+    login: builder.mutation({
+      query: (data) => ({
+        url: '/login',
+        method: 'POST',
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+
+          localStorage.setItem(
+            'auth',
+            JSON.stringify({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          // do nothing
+        }
+      },
+    }),
   }),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
