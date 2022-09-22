653bc2ec1d316e65a89c344192e2115fd960d244
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-22 23:08:31 +0600

    implemented cutom hook 'useAuthCheck' for login persistency

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Conversation from "./pages/Conversation";
-import Inbox from "./pages/Inbox";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import useAuthCheck from './hooks/useAuthCheck';
+import Conversation from './pages/Conversation';
+import Inbox from './pages/Inbox';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
-    );
+  const authCheck = useAuthCheck();
+
+  return !authCheck ? (
+    <div>Authentication Checking</div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
