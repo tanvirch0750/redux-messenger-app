3305125f1e2484959a79c4820fbad113e7223a9f
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-22 19:30:05 +0600

    implemented redux boilerplate with rtk query

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authSliceReducer from '../features/auth/authSlice';
+import conversationsReducer from '../features/conversations/conversationsSlice';
+import messagesReducer from '../features/messages/messagesSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(apiSlice.middleware),
 });
