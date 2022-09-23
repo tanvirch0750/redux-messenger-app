bf8dfb97e232e8dc6096556352eb89abb0cfcfe7
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-23 18:49:29 +0600

    implement messages api functionality

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -1,20 +1,53 @@
 // import Blank from "./Blank";
-import ChatHead from "./ChatHead";
-import Messages from "./Messages";
-import Options from "./Options";
+import { useParams } from 'react-router-dom';
+import { useGetMessagesQuery } from '../../../features/messages/messagesApi';
+import ChatHead from './ChatHead';
+import Messages from './Messages';
+import Options from './Options';
+import Error from '../../ui/Error';
 
 export default function ChatBody() {
-    return (
-        <div className="w-full lg:col-span-2 lg:block">
-            <div className="w-full grid conversation-row-grid">
-                <ChatHead
-                    avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                    name="Akash Ahmed"
-                />
-                <Messages />
-                <Options />
-                {/* <Blank /> */}
-            </div>
-        </div>
+  const { id } = useParams();
+  const { data: messages, isLoading, isError, error } = useGetMessagesQuery(id);
+
+  // decide what to render
+  let content = null;
+  if (isLoading) content = <div className="m-2 text-center">Loading...</div>;
+  if (!isLoading && isError) {
+    content = (
+      <div className="m-2 text-center">
+        <Error message={error?.data} />
+      </div>
     );
+  }
+
+  if (!isLoading && !isError && messages?.length === 0) {
+    content = (
+      <li className="m-2 text-center">
+        <Error message="No messages found" />
+      </li>
+    );
+  }
+
+  if (!isLoading && !isError && messages?.length > 0) {
+    content = (
+      <>
+        <ChatHead
+          avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
+          name="Akash Ahmed"
+        />
+        <Messages messages={messages} />
+        <Options />
+      </>
+    );
+  }
+
+  return (
+    <div className="w-full lg:col-span-2 lg:block">
+      <div className="w-full grid conversation-row-grid">
+        {content}
+        {/* <Blank /> */}
+      </div>
+    </div>
+  );
 }
