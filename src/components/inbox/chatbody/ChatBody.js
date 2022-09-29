0d7ee003dd34a67b99cb25997a386653bb2f9732
tanvirch0750 <tanvir0750@yahoo.com>
2022-09-29 23:12:26 +0600

    implement add message

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -34,7 +34,7 @@ export default function ChatBody() {
       <>
         <ChatHead message={messages[0]} />
         <Messages messages={messages} />
-        <Options />
+        <Options info={messages[0]} />
       </>
     );
   }
